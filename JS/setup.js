cf2d5c6f898161d104b7511bc2e7d3acbd80d3b8
Tamara Costa <tamarasantosc@gmail.com>
2021-03-15 19:41:43 -0300

    Finalizado

--- JS/setup.js
+++ JS/setup.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
     responsive:{
         0:{
             items:1
@@ -14,3 +14,5 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
